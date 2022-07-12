857013d61474109670b1605d6f4bccff70bc741b
thornbird <thornbird.blue@gmail.com>
2022-07-12 17:12:24 +0800

    ci origin code.

---
+++ app/src/main/java/com/example/tb/MyActivity.kt
@@ -0,0 +1,12 @@
+package com.example.tb
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MyActivity : AppCompatActivity() {
+	override fun onCreate(saveInstanceState: Bundle?) {
+		super.onCreate(saveInstanceState)
+
+		setContentView(R.layout.activity_main)
+	}
+}
